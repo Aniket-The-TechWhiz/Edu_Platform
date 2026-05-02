99cd7441a3d08e93c370a99927c8f61d4a992607
Aniket-The-TechWhiz <aniketyelameli@gmail.com>
2026-05-02 11:43:57 +0530

    deleted ai code

--- src/main/java/com/edu_platform/Education_Platform/model/DifficultyLevel.java
+++
@@ -1,8 +0,0 @@
-package com.edu_platform.Education_Platform.model;
-
-public enum DifficultyLevel
-{
-    EASY,
-    MEDIUM,
-    HARD
-}
--- src/main/java/com/edu_platform/Education_Platform/model/Year.java
+++
@@ -1,8 +0,0 @@
-package com.edu_platform.Education_Platform.model;
-
-public enum Year {
-    First_Year,
-    Second_Year,
-    Third_Year,
-    Fourth_Year
-}
